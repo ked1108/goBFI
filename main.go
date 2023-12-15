237901b94ac436e435e28f78d167641ca792a611
Kedar Shinde <kedarshinde8011@gmail.com>
2023-12-15 19:13:39 +0530

    Yay Loops!

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 	"strings"
 )
 
-var code string = ""
+var code = ""
 var data [10000]byte
 var dp uint16 = 0
 
 var isLooping bool
 var nestedLoops uint64
-var loopStack []uint8
+var loopStack []uint64
 
 func opIncVal() {
 	data[dp]++
@@ -33,10 +33,10 @@ func opIncDp() {
 }
 
 func opDecDp() {
-	if int(dp) > 1 {
+	if int(dp) >= 1 {
 		dp--
 	} else {
-		fmt.Println("ERR BUFFER UNDERFLOWED")
+		fmt.Println("ERR BUFFER EMPTY")
 	}
 }
 
@@ -47,14 +47,13 @@ func opIn() {
 		return
 	}
 	data[dp] = inp[0]
-	fmt.Printf("%c\n", inp[0])
 }
 
 func opOut() {
-	fmt.Printf("%c\n", data[dp])
+	fmt.Printf("%c", data[dp])
 }
 
-func loopTest(i int) {
+func loopTest(i uint64) {
 	if isLooping {
 		if code[i] == '[' {
 			nestedLoops++
@@ -68,14 +67,22 @@ func loopTest(i int) {
 	}
 }
 
-func opLoops(i int) int {
+func opOpenLoops(i uint64) {
 	if data[dp] == 0 {
-		idx := loopStack[len(loopStack)-1]
-		if idx >= 0 {
+		isLooping = true
+	} else {
+		loopStack = append(loopStack, i)
+	}
+}
 
+func opCloseLoops(i *uint64) {
+	if data[dp] != 0 {
+		*i = loopStack[len(loopStack)-1]
+	} else {
+		if len(loopStack) > 0 {
+			loopStack = loopStack[:len(loopStack)-1]
 		}
 	}
-	return i
 }
 
 func nop() {
@@ -100,14 +107,15 @@ func cleaner(filename string) {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		// Getting rid of all the whitespace, its a waste anyways
+		// Getting rid of all the whitespace, it's a waste anyway
 		code = code + strings.Replace(scanner.Text(), " ", "", -1)
 	}
 }
 
 func interpreter() {
-
-	for i := 0; i < len(code); i++ {
+	var i uint64
+	for i = 0; i < uint64(len(code)); i++ {
+		loopTest(i)
 		switch code[i] {
 		case '+':
 			opIncVal()
@@ -122,14 +130,13 @@ func interpreter() {
 		case '.':
 			opOut()
 		case '[':
-			i = opLoops(i)
+			opOpenLoops(i)
+		case ']':
+			opCloseLoops(&i)
 		default:
 			nop()
 		}
 	}
-	fmt.Println("")
-	fmt.Println(data[:10])
-
 }
 
 func main() {
